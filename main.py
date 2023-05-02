c5199d1d8e17c5fc3872d9c6e6aa75cabb961203
Naftaly Souza <naftsz.dev@gmail.com>
2023-05-02 11:50:27 -0300

    excessões

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def dimensoesObjeto():
-    altura = float(input('Digite a altura do objeto (em cm): '))
-    comprimento = float(input('Digite o comprimento do objeto (em cm): '))
-    largura = float(input('Digite a largura do objeto (em cm): '))
-    
-    volume = altura * largura * comprimento
+    try: 
+        altura = float(input('Digite a altura do objeto (em cm): '))
+        comprimento = float(input('Digite o comprimento do objeto (em cm): '))
+        largura = float(input('Digite a largura do objeto (em cm): '))
+        
+        volume = altura * largura * comprimento
+        
+    except ValueError:
+        print('Você digitou alguma dimensão do objeto com valor não numérico')
+        print('Por favor, entre com as dimensões desejadas novamente.')
