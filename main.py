8c78728226b63dd482b681132864ce01f8adf39d
Naftaly Souza <naftsz.dev@gmail.com>
2023-05-02 11:44:20 -0300

    EXigencia 1 de 3: incompleto

---
+++ main.py
@@ -0,0 +1,6 @@
+def dimensoesObjeto():
+    altura = float(input('Digite a altura do objeto (em cm): '))
+    comprimento = float(input('Digite o comprimento do objeto (em cm): '))
+    largura = float(input('Digite a largura do objeto (em cm): '))
+    
+    volume = altura * largura * comprimento
